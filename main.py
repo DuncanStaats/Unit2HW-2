3821eb070af5260f2597b641e0d7f2f76eb1474a
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2024-10-03 16:54:32 +0000

    new

---
+++ main.py
@@ -0,0 +1,37 @@
+print("Welcome to a Priate's life Mad Lib!".center(60,'-'))
+
+User_name = input("Enter in your name: ") #print(user_name.title)
+Date = input("Enter in a month, date, and year(mm/dd/yy): ")
+print("I will now ask you for 5 nouns, 5 verbs, 3 adjectives, and 2 adverbs")
+
+print("I will first ask you for the nouns!")
+noun_1 = input("Enter in the first noun please: ")
+noun_2 = input("Enter in the second noun please: ")
+noun_3 = input("Enter in the third noun please: ")
+noun_4 = input("Enter in the fouth noun please: ")
+noun_5 = input("Enter in the fifth noun please: ")
+
+print("I will now ask you for the verbs!")
+verb_1 = input("Enter in the first verb please: ")
+verb_2 = input("Enter in the second verb please: ")
+verb_3 = input("Enter in the third verb please: ")
+verb_4 = input("Enter in the fourth verb please: ")
+verb_5 = input("Enter in the fifth verb please: ")
+
+print("Now I will ask you for the adjectives!")
+adjective_1 = input("Enter in the first adjective please: ")
+adjective_2 = input("Enter in the second adjective please: ")
+adjective_3 = input("Enter in the third adjective please: ")
+
+print("Finally I will ask you for the adverbs!")
+adverb_1 = input("Enter in the first adverb please: ")
+adverb_2 = input("Enter in the second adverb please: ")
+
+print(f"Now I will tell you the story of Captain {noun_1}'s life.")
+print(f"Once upon a time, there was a fearless pirate named Captain {noun_1). 
+      He sailed the treacherous seas on his ship, the (noun). 
+      Every morning, he would (verb) with excitement as he prepared for the day ahead. 
+      His crew, made up of (adjective) sailors, would gather to (verb) their weapons and (verb) for adventure.
+      One day, they discovered a hidden (noun) on a distant island. 
+      The treasure was said to be guarded by a (adjective) beast that roamed the land (adverb). 
+      Undeterred, Captain (noun) and his crew set off to claim their fortune!")
